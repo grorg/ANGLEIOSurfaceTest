a839c270a565034d3570f518ad28ec5ccf8449b9
Dean Jackson <dino@apple.com>
2018-08-03 08:18:30 +1000

    Initial Commit

---
+++ ANGLEIOSurfaceTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ANGLEIOSurfaceTest
+//
+//  Created by dino on 3/8/18.
+//  Copyright © 2018 dino. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ANGLEIOSurfaceTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ANGLEIOSurfaceTest
+//
+//  Created by dino on 3/8/18.
+//  Copyright © 2018 dino. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
